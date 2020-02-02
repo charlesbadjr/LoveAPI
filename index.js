c3968a10ac29e5da1d4df54519ab40fd7fd3582e
Charlie <charlesbadjr@gmail.com>
2020-02-02 02:48:03 -0700

    mermaid crud

--- index.js
+++ index.js
@@ -37,19 +37,79 @@ app.post('/api/create', (req, res)=> {
 
 
 
-// Read Crud Action
+// Read ITEM  Crud Action
+
+app.get('/api/read/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            let item = await document.get();
+            let response = item.data();
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+// read all Crud action 
+
+app.get('/api/read', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('items');
+            let response = [];
+            await query.get().then(querySnapshot => {
+            let docs = querySnapshot.docs;
+            for (let doc of docs) {
+                const selectedItem = {
+                    id: doc.id,
+                    item: doc.data().item
+                };
+                response.push(selectedItem);
+            }
+            });
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
 
 
+// Update ITEM Crud Action
 
+app.put('/api/update/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            await document.update({
+                item: req.body.item
+            });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+    
+// Destroy Item Crud Action 
 
-// Update Crud Action
-
-
-
-
-// Destroy Crud Action
-
-
+    app.delete('/api/delete/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            await document.delete();
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
 
 
 exports.app = functions.https.onRequest(app);
